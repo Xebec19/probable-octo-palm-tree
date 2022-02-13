0a357dfde1160ff01121845fb704364e160c7278
xebec19 <rohant946@gmail.com>
2022-02-13 23:45:18 +0530

    revising props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 const App = () => {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.23, date: new Date(2021, 2, 28) },
+    { title: "Car Insurance", amount: 245.48, date: new Date(2021, 2, 2) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title}></ExpenseItem>
+      {/* <ExpenseItem></ExpenseItem> */}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
-const ExpenseItem = () => {
-    return <h1>Expense Item!</h1>
-}
+import "./ExpenseItem.css";
+const ExpenseItem = (props) => {
+  const { title } = props;
+  const expenseDate = new Date();
+  return (
+    <div className="expense-item">
+      <span className="expense-item__start">
+        <h1 className="expense-item__title">{title}</h1>
+      </span>
+      <span className="expense-item__end">
+        <h1 className="button">$43</h1>
+        <h2 className="expense-item__date">{expenseDate.toISOString()}</h2>
+      </span>
+    </div>
+  );
+};
 
 export default ExpenseItem;
