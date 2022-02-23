0fc98332a74e10ce16ef347b39460270dfff2157
xebec19 <rohant946@gmail.com>
2022-02-23 21:09:08 +0530

    reusable wrapper component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import "./ExpenseItem.css";
+import Card from './Card.js';
 const ExpenseItem = (props) => {
   const { title } = props;
   const expenseDate = new Date();
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <span className="expense-item__start">
         <h1 className="expense-item__title">{title}</h1>
       </span>
@@ -11,7 +12,7 @@ const ExpenseItem = (props) => {
         <h1 className="button">$43</h1>
         <h2 className="expense-item__date">{expenseDate.toISOString()}</h2>
       </span>
-    </div>
+    </Card>
   );
 };
 
