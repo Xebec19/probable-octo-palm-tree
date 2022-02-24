091a08c1fc654700e99097924584f118bb91a903
xebec19 <rohant946@gmail.com>
2022-02-24 23:11:45 +0530

    save

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import "./ExpenseItem.css";
 import Card from './Card.js';
 const ExpenseItem = (props) => {
   const { title } = props;
   const expenseDate = new Date();
+
+  const [titleString,setTitle] = useState(title);
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+  }
+
   return (
     <Card className="expense-item">
       <span className="expense-item__start">
-        <h1 className="expense-item__title">{title}</h1>
+        <h1 className="expense-item__title">{titleString}</h1>
       </span>
       <span className="expense-item__end">
         <h1 className="button">$43</h1>
         <h2 className="expense-item__date">{expenseDate.toISOString()}</h2>
       </span>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
